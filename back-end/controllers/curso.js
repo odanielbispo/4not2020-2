031a54af61ddb166cb9f215742cb478f797ee424
Daniel Bispo <danieubispo@gmail.com>
2020-09-11 01:29:53 +0000

    (10/09) Métodos RETRIEVE no controller

--- back-end/controllers/curso.js
+++ back-end/controllers/curso.js
@@ -2,6 +2,7 @@ const Curso = require('../models/Curso')
 
 const controller = {}
 
+//OPERAÇÃO CREATE, funcção novo()
 controller.novo = async (req, res) => {
     // Usa os dados que chega dentro do body da requisição
     // e depois envia ao BD para a criação de um novo objeto
@@ -16,5 +17,37 @@ controller.novo = async (req, res) => {
         res.status(500).send(erro)
     }
 } 
+
+//OPERAÇÃO RETRIEVE (all), função listar()
+
+controller.listar = async (req, res) => {
+    try{
+        let dados = await Curso.find() //traz todos os cursos cadastrados
+        res.send(dados)
+    }
+    catch(erro){
+        console.log(erro)
+        res.status(500).send(erro)
+    }
+}
+
+//OPERAÇÃO RETRIEVE (one), função obterUm()
+
+controller.obterUm = async (req, res) =>{
+    try{
+        //capturando o parametro ID da url
+        const id = req.params.id
+        let obj = await Curso.findById(id)
+
+        //o objeto existe e foi encontrato
+        if(obj) res.send(obj)      //HTTP 200
+        //Não encontrado
+        else res.status(404).end() //HTTP 404: Not found
+    }
+    catch(erro){
+        console.log(erro)
+        res.status(500).send(erro)
+    }
+}
 
 module.exports = controller
